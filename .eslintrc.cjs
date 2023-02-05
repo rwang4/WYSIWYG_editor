9e1f0b467fd98ecc2c054a4adb3ab10809572b88
rwang4 <rwang4@ualberta.ca>
2023-02-05 22:23:54 +0000

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": ["off"],
+    "react/jsx-no-target-blank": ["off"]
+  },
+  ignorePatterns: ["**/*.css", "**/*.scss", "**/*.html"]
+};
