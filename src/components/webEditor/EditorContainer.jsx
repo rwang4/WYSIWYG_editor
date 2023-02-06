369dfb3603ea55792323b1c7ccec9d2c2a5dbef3
rwang4 <rwang4@ualberta.ca>
2023-02-06 23:03:51 +0000

    draggable added

--- src/components/webEditor/EditorContainer.jsx
+++ src/components/webEditor/EditorContainer.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import './EditorContainer.css';
 import ModeContext from '../../ModeContext';
 import EditorTile from './editorTile/EditorTile';
--- src/components/webEditor/editorTile/EditorTile.jsx
+++ src/components/webEditor/editorTile/EditorTile.jsx
@@ -1,8 +1,34 @@
+import { useState } from 'react';
+
 import './EditorTile.css';
 
 function EditorTile(props) {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [xAxis, setXAxis] = useState(0);
+  const handleDragStart = (event) => {
+    // This method runs when the dragging starts
+    console.log('Started');
+  };
+  const handleDrag = (event) => {
+    setXAxis(event.clientX);
+    console.log(xAxis);
+  };
+  const handleDragEnd = (event) => {
+    setX(event.clientX);
+    setY(event.clientY);
+    setXAxis(0);
+    console.log(x, y);
+  };
+
   return (
-    <div className="editor-tile" title="Text" draggable="true">
+    <div
+      className="editor-tile"
+      title="Text"
+      draggable
+      onDragStart={handleDragStart}
+      onDrag={handleDrag}
+      onDragEnd={handleDragEnd}>
       <div className="editor-tile-media">
         <svg viewBox="0 0 24 24">
           <path fill="currentColor" d={props.svgPath}></path>
