e068f43684115ea42c76ad6f646c2ac7d67105c7
rwang4 <rwang4@ualberta.ca>
2023-02-08 01:46:18 +0000

    windowSize + editbutton

--- src/components/custom/CustomApp.jsx
+++ src/components/custom/CustomApp.jsx
@@ -1,37 +1,105 @@
-import { useState, useEffect } from 'react';
-import useModeContext from '../../contexts/ModeContext';
+import { useState, useEffect, useRef } from 'react';
 import './CustomApp.css';
+import { textDefault, buttonDefault } from '../../data/ComponentData';
 import EditButton from './edit/EditButton';
+import { checkOverflow } from '../../contexts/functions';
+import { WindowContextProvider } from '../../contexts/WindowContext';
 
 function CustomApp() {
+  const txtCount = useRef(0);
+  const btnCount = useRef(0);
+  const imgCount = useRef(0);
+  const inputCount = useRef(0);
+  const formCount = useRef(0);
+  const [clientSize, setClientSize] = useState([window.innerWidth, window.innerHeight]);
   const [editComponentList, setEditComponentList] = useState([]);
-  const [dataObj, setDataObj] = useState({});
-  const { isEdit } = useModeContext();
-  console.log(isEdit);
+  const [previewComponentList, setPreviewComponentList] = useState([]);
+  const [isMode, setMode] = useState(true);
   const addEditComponent = (comp) => {
-    setEditComponentList(editComponentList.concat(comp));
+    setEditComponentList([...editComponentList, comp]);
+  };
+  const addPreviewComponent = (comp) => {
+    setPreviewComponentList([...previewComponentList, comp]);
+  };
+  const changeMode = () => {
+    setMode(!isMode);
   };
   const eventHandler = (event) => {
-    // if (e.origin !== 'http://localhost:5173') return;
-    setDataObj(JSON.parse(event.data));
-    switch (dataObj.type) {
-      case 'Text':
-        break;
-      case 'Button':
-        addEditComponent(<EditButton x={dataObj.x} y={dataObj.y} />);
-        break;
-      case 'Image':
-        break;
-      case 'Input':
-        break;
-      case 'Form':
-        break;
+    if (event.data == 'modeChange') {
+      changeMode();
+    } else {
+      const dataObj = JSON.parse(event.data);
+      switch (dataObj.type) {
+        case 'Text':
+          txtCount.current += 1;
+          break;
+        case 'Button':
+          btnCount.current += 1;
+          if (
+            checkOverflow(
+              dataObj.x,
+              dataObj.y,
+              buttonDefault.width,
+              buttonDefault.height,
+              clientSize
+            )
+          ) {
+            break;
+          }
+          addEditComponent({
+            type: 'btn',
+            comp: (
+              <EditButton
+                key={`btn${btnCount.current}`}
+                x={dataObj.x - buttonDefault.width / 2}
+                y={dataObj.y - buttonDefault.height / 2}
+              />
+            )
+          });
+          break;
+        case 'Image':
+          imgCount.current += 1;
+          break;
+        case 'Input':
+          inputCount.current += 1;
+          break;
+        case 'Form':
+          formCount.current += 1;
+          break;
+        default:
+          break;
+      }
     }
   };
+
   useEffect(() => {
     window.addEventListener('message', eventHandler, false);
-  }, []);
-  return <div className="custom-app"></div>;
+    return () => {
+      window.removeEventListener('message', eventHandler);
+    };
+  });
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setClientSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
+  const editDisplayComponents = editComponentList.map((c) => {
+    return c.comp;
+  });
+  useEffect(() => {}, [clientSize]);
+  return (
+    <div className="custom-app">
+      <WindowContextProvider>
+        {isMode ? editDisplayComponents : previewComponentList}
+      </WindowContextProvider>
+    </div>
+  );
 }
 
 export default CustomApp;
